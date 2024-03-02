99f3f11868055356a3219fe2d4d6223eb531e611
Michael Stepanishchevskiy <mihail.mike@mail.ru>
2024-03-02 17:59:02 +0300

    second iteration

--- ftracker.go
+++ ftracker.go
@@ -102,7 +102,8 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	calories := (walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed(action, duration), 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH
+	walkingSpeed := meanSpeed(action, duration) * kmhInMsec
+	calories := (walkingCaloriesWeightMultiplier*weight + math.Pow(walkingSpeed, 2)/height*cmInM*walkingSpeedHeightMultiplier*weight) * duration * minInH
 	return calories
 }
 
@@ -136,6 +137,6 @@ func swimmingMeanSpeed(lengthPool, countPool int, duration float64) float64 {
 // weight float64 — вес пользователя.
 func SwimmingSpentCalories(lengthPool, countPool int, duration, weight float64) float64 {
 	// ваш код здесь
-	calories := (swimmingMeanSpeed(lengthPool, countPool, duration) + swimmingCaloriesMeanSpeedShift) * swimmingCaloriesWeightMultiplier * duration
+	calories := (swimmingMeanSpeed(lengthPool, countPool, duration) + swimmingCaloriesMeanSpeedShift) * swimmingCaloriesWeightMultiplier * weight * duration
 	return calories
 }
